fd35e844ef5f39fa4653819cd6079b137675fcfd
ALight Technology And Services Limited <95095695+ALightTechnologyAndServicesLimited@users.noreply.github.com>
2022-06-23 16:21:53 +0100

    Initial A

---
+++ NLogSampleDemo001/NLogSampleDemo001/MyClass.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogSampleDemo001
+{
+    public class MyClass : IMyInterface
+    {
+        private readonly ILogger<MyClass> logger;
+
+        public MyClass(ILogger<MyClass> logger)
+        {
+            this.logger = logger;
+            logger.LogDebug("Constructor Invoked.");
+        }
+
+        public void PrintHello()
+        {
+            logger.LogDebug("PrintHello Invoked.");
+            Console.WriteLine("Hello!");
+        }
+    }
+}
---
+++ NLogSampleDemo001/NLogSampleDemo001/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+using NLogSampleDemo001;
+
+var logger = LogManager.GetCurrentClassLogger();
+
+try
+{
+    var environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+    var configBuilder = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+    var nlogConfigFileName = "nlog.config";
+
+    if (!String.IsNullOrEmpty(environment))
+    {
+        configBuilder.AddJsonFile($"appsettings.json", optional: true, reloadOnChange: true);
+        nlogConfigFileName = $"nlog.{environment}.config";
+    }
+
+    var config = configBuilder.Build();
+
+
+    var servicesProvider = new ServiceCollection()
+        .AddTransient<IMyInterface, MyClass>()
+       // Add Additional Services i.e bind interfaces to classes
+       .AddLogging(loggingBuilder =>
+       {
+           // configure Logging with NLog
+           loggingBuilder.ClearProviders();
+           loggingBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+           loggingBuilder.AddNLog(nlogConfigFileName);
+       })
+       .BuildServiceProvider();
+
+    using(servicesProvider as IDisposable)
+    {
+        var myInterface = servicesProvider.GetRequiredService<IMyInterface>();
+        myInterface.PrintHello();
+    }
+}
+catch (Exception e)
+{
+    logger.Error(e, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    LogManager.Shutdown();
+}
